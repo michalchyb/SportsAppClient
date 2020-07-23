21d3228e7daf9134a45762c1fbf359d913d76d58
Michał Chyb <michalchyb1@gmail.com>
2020-07-23 15:04:01 +0200

    # fix token storage # add profile logic # move login service from home to header

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { TokenStorageService } from '../services/token-storage.service';
 
 @Component({
   selector: 'app-home',
@@ -7,29 +6,9 @@ import { TokenStorageService } from '../services/token-storage.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-
-  private roles: string[];
-  isLoggedIn = false;
-  showAdminBoard = false;
-  showModeratorBoard = false;
-  username: string;
-
-  constructor(
-    private tokenStorageService: TokenStorageService) { }
+  
+  constructor( ) { }
 
   ngOnInit(): void {
-    this.isLoggedIn = !!this.tokenStorageService.getToken();
-
-    if (this.isLoggedIn) {
-      const user = this.tokenStorageService.getUser();
-      this.roles = user.roles;
-      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
-      this.username = user.username;
-    }
-  }
-  logout() {
-    this.tokenStorageService.signOut();
-    window.location.reload();
   }
 }
--- src/app/services/token-storage.service.ts
+++ src/app/services/token-storage.service.ts
@@ -16,20 +16,19 @@ export class TokenStorageService {
 
   public saveToken(token: string) {
     window.sessionStorage.removeItem(TOKEN_KEY);
-    window.sessionStorage.setItem(USER_KEY, token);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
   }
 
-  public getToken() {
+  public getToken(): string {
     return sessionStorage.getItem(TOKEN_KEY);
   }
 
   public saveUser(user) {
-    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.removeItem(USER_KEY);
     window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
   }
 
   public getUser() {
     return JSON.parse(sessionStorage.getItem(USER_KEY));
   }
-  
 }
